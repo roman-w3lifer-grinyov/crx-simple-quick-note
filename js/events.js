78f15418db1a3f90f8b99862f87770c623084f74
Roman Grinyov <w3lifer@gmail.com>
2021-11-19 20:29:01 +0300

    removed `onStartup` & `onUpdated` listeners

--- js/events.js
+++ js/events.js
@@ -2,11 +2,6 @@
 
 /* global chrome */
 
-chrome.runtime.onInstalled.addListener(onInstalledAndOnStartupListener);
-chrome.runtime.onStartup.addListener(onInstalledAndOnStartupListener);
-chrome.tabs.onUpdated.addListener(() => onInstalledAndOnStartupListener);
-
-function onInstalledAndOnStartupListener()
-{
-  chrome.storage.sync.get('note', storage => storage.note && setBadge(storage.note));
-}
+chrome.runtime.onInstalled.addListener(
+  () => chrome.storage.sync.get('note', storage => storage.note && setBadge(storage.note))
+);
