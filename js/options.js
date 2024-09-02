ee4eb1894115c4a7cc83c8ebb2644565015375e6
Roman Grinyov <w3lifer@gmail.com>
2024-09-02 21:59:02 +0300

    3.5.0: added `Side panel` mode

--- js/options.js
+++ js/options.js
@@ -4,16 +4,22 @@ window.addEventListener('DOMContentLoaded', _ => {
 
   const textareaHeightElement = document.getElementById('textarea-height')
   const textareaWidthElement = document.getElementById('textarea-width')
+  const sidePanelModeElement = document.getElementById('side-panel-mode')
 
   chrome.storage.sync.get(null, storage => {
-    const height = storage.textareaHeight || app.textareaHeight
-    const width = storage.textareaWidth || app.textareaWidth
-    textareaHeightElement.value = height
-    textareaWidthElement.value = width
+    textareaHeightElement.value = storage.textareaHeight || app.textareaHeight
+    textareaWidthElement.value = storage.textareaWidth || app.textareaWidth
+    sidePanelModeElement.checked = storage.sidePanelMode || app.sidePanelMode
   })
 
   textareaHeightElement.addEventListener('input', e => chrome.storage.sync.set({textareaHeight: e.target.value}))
   textareaWidthElement.addEventListener('input', e => chrome.storage.sync.set({textareaWidth: e.target.value}))
+  sidePanelModeElement.addEventListener('change', e => {
+    chrome.storage.sync.set({sidePanelMode: e.target.checked})
+    e.target.checked
+      ? chrome.sidePanel.setPanelBehavior({openPanelOnActionClick: true})
+      : chrome.sidePanel.setPanelBehavior({openPanelOnActionClick: false})
+  })
 
   document.getElementById('close-button').addEventListener('click', _ => close())
 
--- js/service-worker.js
+++ js/service-worker.js
@@ -3,6 +3,13 @@
 chrome.runtime.onInstalled.addListener(
   _ => chrome.storage.sync.get(
     null,
-    storage => chrome.action.setBadgeText({'text': storage.note && '' + storage.note.length})
+    storage => {
+      chrome.action.setBadgeText({'text': storage.note && '' + storage.note.length})
+      chrome.storage.sync.get(null, storage => {
+        if (!storage.sidePanelMode) {
+          chrome.sidePanel.setPanelBehavior({openPanelOnActionClick: false})
+        }
+      })
+    }
   )
 )
