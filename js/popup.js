e869ef07687e2d90f07c3005ddeb00f648660979
Roman Grinyov <w3lifer@gmail.com>
2024-09-04 23:02:39 +0300

    options in popup: initial commit

--- js/popup.js
+++ js/popup.js
@@ -31,21 +31,18 @@ window.addEventListener('DOMContentLoaded', _ => {
 
   copyButton.addEventListener('click', _ => {
     copyTextToClipboard(textarea.value)
-    showNotification('Done!')
+    showNotification('Copied!')
   })
 
   cutButton.addEventListener('click', _ => {
     copyTextToClipboard(textarea.value)
     clearNote()
-    showNotification('Done!')
   })
 
   resetButton.addEventListener('click', _ => {
     if (confirm('Your note will be deleted!')) {
       clearNote()
-      showNotification('Done!')
     }
-
   })
 
   textarea.addEventListener('selectionchange', _ =>
@@ -73,5 +70,69 @@ window.addEventListener('DOMContentLoaded', _ => {
     notificationElement.style.visibility = 'visible'
     setTimeout(_ => notificationElement.style.visibility = 'hidden', 1000)
   }
+
+  /*
+   * ===================================================================================================================
+   * OPTIONS
+   * ===================================================================================================================
+   */
+
+  const optionsLink = document.getElementById('options-link')
+  const optionsContainer = document.getElementById('options-container')
+  optionsLink.addEventListener('click', _ => {
+    if (optionsContainer.classList.toggle('hidden')) {
+      optionsLink.textContent = 'Show options'
+    } else {
+      optionsLink.textContent = 'Hide options'
+    }
+  })
+  chrome.storage.local.get(null, storage => {
+    if (storage.openOptions) {
+      optionsLink.click()
+      chrome.storage.local.set({openOptions: false})
+    }
+  })
+
+  const textareaHeightElement = document.getElementById('textarea-height')
+  const textareaWidthElement = document.getElementById('textarea-width')
+  const sidePanelModeElement = document.getElementById('side-panel-mode')
+
+  chrome.storage.sync.get(null, storage => {
+    textareaHeightElement.value = storage.textareaHeight || app.textareaHeight
+    textareaWidthElement.value = storage.textareaWidth >= 338 ? storage.textareaWidth : app.textareaWidth
+    sidePanelModeElement.checked = storage.sidePanelMode || app.sidePanelMode
+  })
+
+  textareaHeightElement.addEventListener('input', e => {
+    if (e.target.value > 440) {
+      e.target.value = 440
+    } else if (('' + e.target.value).length === 2 && e.target.value < 16) {
+      e.target.value = 16
+    }
+    textarea.style.height = e.target.value + 'px'
+    chrome.storage.sync.set({textareaHeight: e.target.value})
+  })
+  textareaWidthElement.addEventListener('input', e => {
+    if (e.target.value > 700) {
+      e.target.value = 700
+    } else if (('' + e.target.value).length === 3 && e.target.value < 338) {
+      e.target.value = 338
+    }
+    textarea.style.width = e.target.value + 'px'
+    chrome.storage.sync.set({textareaWidth: e.target.value})
+  })
+  sidePanelModeElement.addEventListener('change', e => {
+    chrome.storage.sync.set({sidePanelMode: e.target.checked})
+    if (e.target.checked) {
+      close()
+      chrome.tabs.query({active: true, currentWindow: true}, tabs => chrome.sidePanel.open({tabId: tabs[0].id}))
+      chrome.sidePanel.setPanelBehavior({openPanelOnActionClick: true})
+    } else {
+      close()
+      chrome.sidePanel.setPanelBehavior({openPanelOnActionClick: false})
+      chrome.action.openPopup()
+    }
+    chrome.storage.local.set({openOptions: true})
+  })
 
 })
